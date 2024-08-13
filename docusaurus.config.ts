45acc29b78aae90b0236f48f17ed8cceb35f8e47
Damien J. Burks <damienjburks@Damiens-MacBook-Pro.local>
2024-08-12 20:41:48 -0500

    testing output page

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,8 +3,8 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'DSB',
+  tagline: 'DevSec Blueprint',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
