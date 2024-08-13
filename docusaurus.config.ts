fe7a95b262eeb64408ffa47be388bbabf45871f7
Damien J. Burks <damienjburks@Damiens-MacBook-Pro.local>
2024-08-12 22:33:09 -0500

    updaitng config file

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -38,7 +38,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/The-DevSec-Blueprint/devsecblueprint.github.io',
         },
         blog: {
           showReadingTime: true,
@@ -49,7 +49,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+'https://github.com/The-DevSec-Blueprint/devsecblueprint.github.io'
         },
         theme: {
           customCss: './src/css/custom.css',
@@ -76,7 +76,7 @@ const config: Config = {
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/The-DevSec-Blueprint/devsecblueprint.github.io',
           label: 'GitHub',
           position: 'right',
         },
